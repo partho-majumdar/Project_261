6b40717788e26ac937d671ed3220c56d1b16a070
heipartho05 <heipartho72@gmail.com>
2026-04-20 23:11:01 +0600

    Login Logout Api added

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -21,8 +21,39 @@ export const registerUser = asyncHandler(async (req, res, next) => {
   generateToken(user, 201, "User registered successfully", res);
 });
 
-export const login = asyncHandler(async (req, res, next) => {});
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password, role } = req.body;
+
+  if (!email || !password || !role) {
+    return next(new ErrorHandler("Please provide all required fields", 400));
+  }
+  const user = await User.findOne({ email, role }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email, password or role", 401));
+  }
+
+  const isPasswordMatch = await user.comparePassword(password);
+
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid email, password or role", 401));
+  }
+  generateToken(user, 200, "Logged in successfully", res);
+});
+
+export const logout = asyncHandler(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully",
+    });
+});
+
 export const getUser = asyncHandler(async (req, res, next) => {});
-export const logout = asyncHandler(async (req, res, next) => {});
 export const forgotPassword = asyncHandler(async (req, res, next) => {});
 export const resetPassword = asyncHandler(async (req, res, next) => {});
--- Server/models/user.js
+++ Server/models/user.js
@@ -78,11 +78,21 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
 userSchema.methods.generateToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
   });
 };
 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
