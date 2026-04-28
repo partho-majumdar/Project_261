cffe650febc5804b0a69e4f2251ac2e789a18a9e
heipartho05 <heipartho72@gmail.com>
2026-04-29 01:07:34 +0600

    add forgot and reset password api

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,7 +1,10 @@
-import { asyncHandler } from "../middleware/asyncHandler.js";
-import ErrorHandler from "../middleware/error.js";
+import { asyncHandler } from "../middlewares/asyncHandler.js";
+import ErrorHandler from "../middlewares/error.js";
 import { User } from "../models/user.js";
+import { sendEmail } from "../services/emailService.js";
+import { generateForgotPasswordEmailTemplate } from "../utils/emailTemplates.js";
 import { generateToken } from "../utils/generateToken.js";
+import crypto from "crypto";
 
 export const registerUser = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -27,15 +30,16 @@ export const login = asyncHandler(async (req, res, next) => {
   if (!email || !password || !role) {
     return next(new ErrorHandler("Please provide all required fields", 400));
   }
+
   const user = await User.findOne({ email, role }).select("+password");
 
   if (!user) {
     return next(new ErrorHandler("Invalid email, password or role", 401));
   }
 
-  const isPasswordMatch = await user.comparePassword(password);
+  const isPasswordMatched = await user.comparePassword(password);
 
-  if (!isPasswordMatch) {
+  if (!isPasswordMatched) {
     return next(new ErrorHandler("Invalid email, password or role", 401));
   }
   generateToken(user, 200, "Logged in successfully", res);
@@ -54,6 +58,80 @@ export const logout = asyncHandler(async (req, res, next) => {
     });
 });
 
-export const getUser = asyncHandler(async (req, res, next) => {});
-export const forgotPassword = asyncHandler(async (req, res, next) => {});
-export const resetPassword = asyncHandler(async (req, res, next) => {});
+export const getUser = asyncHandler(async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found with this email", 404));
+  }
+
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`;
+
+  const message = generateForgotPasswordEmailTemplate(resetPasswordUrl);
+
+  try {
+    await sendEmail({
+      to: user.email,
+      subject: "FYP SYSTEM - 🔐 Password Reset Request",
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message || "Cannot send email", 500));
+  }
+});
+
+export const resetPassword = asyncHandler(async (req, res, next) => {
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler("Invalid or expired password reset token", 400),
+    );
+  }
+
+  if (!req.body.password || !req.body.confirmPassword) {
+    return next(new ErrorHandler("Please provide all required fields", 400));
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(
+      new ErrorHandler("Password and Confirm Password do not match", 400),
+    );
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  generateToken(user, 200, "Password reset successful", res);
+});
