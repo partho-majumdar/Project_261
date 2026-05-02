43d40a2e95e1bbf3e3f4b501af6ef3c82ad73769
heipartho05 <heipartho72@gmail.com>
2026-05-02 11:56:45 +0600

    backend json file

--- Server/server.js
+++ Server/server.js
@@ -1,29 +1,22 @@
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
-//----------------- Database Connection -----------------
 connectDB();
 
-//----------------- Start Server -----------------
 const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-//----------------- Error Handling -----------------
-process.on("unhandledRejection", (error) => {
-  console.error(`Unhandled Rejection: ${error.message}`);
-  server.close(() => {
-    process.exit(1);
-  });
+process.on("unhandledRejection", (err) => {
+  console.error(`Unhandled Rejection: ${err.message}`);
+  server.close(() => process.exit(1));
 });
 
-process.on("uncaughtException", (error) => {
-  console.error(`Uncaught Exception: ${error.message}`);
+process.on("uncaughtException", (err) => {
+  console.error(`Uncaught Exception: ${err.message}`);
   process.exit(1);
 });
 
 export default server;
-
-
